70b14e90bcdf83dc7729daf969cd587d4170e752
Zack <hickmanzackary@gmail.com>
2018-09-18 14:29:35 -0500

    added shuffle

--- index.js
+++ index.js
@@ -200,6 +200,31 @@ io.on('connection', function(socket){
     socket.on('player-update', function(data){
         //calc and let everyone know whats up
     })
+    socket.on('shuffle-playlist', function(){
+        function shuffle(array) {
+            var currentIndex = array.length, temporaryValue, randomIndex;
+          
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
+          
+              // Pick a remaining element...
+              randomIndex = Math.floor(Math.random() * currentIndex);
+              currentIndex -= 1;
+          
+              // And swap it with the current element.
+              temporaryValue = array[currentIndex];
+              array[currentIndex] = array[randomIndex];
+              array[randomIndex] = temporaryValue;
+            }
+          
+            return array;
+          }
+          
+          // Used like so
+          currentQueue = shuffle(currentQueue);
+          console.log(currentQueue);
+          sendQueue()
+    })
     socket.on('player-ready', function(data){
         setPlayerVolume(socket)
         player.device_id = [data]
